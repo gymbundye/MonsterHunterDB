63172e0aadc102b8f75145603dca0cc093f5d90d
gymbundye <84460968+gymbundye@users.noreply.github.com>
2023-11-14 10:53:24 -0500

    wrote create function

---
+++ MonsterHunterDB/Pages/Index.razor.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+namespace MonsterHunterDB.Pages
+{
+
+	public partial class Index
+	{
+        public bool ShowCreate { get; set; }
+    }
+    protected override async Task OnInitializedAsync()
+    {
+        ShowCreate = false;
+    }
+
+    private MonsterDataContext? _context;
+    public Monster? NewMonter { get; set; }
+
+    public void ShowCreateForm()
+    {
+        NewMonster = new Monster();
+        ShowCreate = true;
+    }
+    public async Task CreateNewMonster()
+    {
+        _context ??= await MonsterDataContextFactory.CreateDbContextAsync();
+
+        if (NewMonster is not null)
+        {
+            _context?.Monsters.Add(NewMonster);
+            _context?.SaveChangesAsync();
+        }
+        ShowCreate = false;
+    }
+
+
+}
