e8280f30fb89374881514acdcff1feba02dc2761
gymbundye <84460968+gymbundye@users.noreply.github.com>
2023-11-14 23:31:15 -0500

    worked on create method

--- MonsterHunterDB/Data/Monster.cs
+++ MonsterHunterDB/Data/Monster.cs
@@ -4,7 +4,7 @@
 	public class Monster
 	{
 		public int Id { get; set; }
-		public string FirstName { get; set; }
+		public string? FirstName { get; set; }
 		public string LastName { get; set; }
 		public string MonsterName { get; set; }
 		public string Type { get; set; }
--- MonsterHunterDB/Pages/Index.razor.cs
+++ MonsterHunterDB/Pages/Index.razor.cs
@@ -1,36 +1,40 @@
-﻿using System.Threading.Tasks;
+﻿using MonsterHunterDB.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace MonsterHunterDB.Pages
 {
 
-	public partial class Index
-	{
+    public partial class Index
+    {
         public bool ShowCreate { get; set; }
-    }
-    protected override async Task OnInitializedAsync()
-    {
-        ShowCreate = false;
-    }
 
-    private MonsterDataContext? _context;
-    public Monster? NewMonter { get; set; }
+        private MonsterDataContext? _context;
+        public Monster? NewMonster { get; set; }
 
-    public void ShowCreateForm()
-    {
-        NewMonster = new Monster();
-        ShowCreate = true;
-    }
-    public async Task CreateNewMonster()
-    {
-        _context ??= await MonsterDataContextFactory.CreateDbContextAsync();
-
-        if (NewMonster is not null)
+        protected override async Task OnInitializedAsync()
         {
-            _context?.Monsters.Add(NewMonster);
-            _context?.SaveChangesAsync();
+            ShowCreate = false;
         }
-        ShowCreate = false;
-    }
 
+
+
+        public void ShowCreateForm()
+        {
+            NewMonster = new Monster();
+            ShowCreate = true;
+        }
+        public async Task CreateNewMonster()
+        {
+            _context ??= await MonsterDataContextFactory.CreateDbContextAsync();
+
+            if (NewMonster is not null)
+            {
+                _context?.Monsters.Add(NewMonster);
+                _context?.SaveChangesAsync();
+            }
+            ShowCreate = false;
+        }
+    }
 
 }
