59d26cc9365528a9f1fcb56e37c5d40cb21403b9
gymbundye <84460968+gymbundye@users.noreply.github.com>
2023-11-16 17:33:39 -0500

    added read method

--- MonsterHunterDB/Pages/Index.razor.cs
+++ MonsterHunterDB/Pages/Index.razor.cs
@@ -12,9 +12,12 @@ namespace MonsterHunterDB.Pages
         private MonsterDataContext? _context;
         public Monster? NewMonster { get; set; }
 
+        public List<Monster>? OurMonsters { get; set; }   
+
         protected override async Task OnInitializedAsync()
         {
             ShowCreate = false;
+            await ShowMonsters();
         }
 
 
@@ -24,6 +27,8 @@ namespace MonsterHunterDB.Pages
             NewMonster = new Monster();
             ShowCreate = true;
         }
+
+        // This Method adds a New Monster to the Database an example of Create//
         public async Task CreateNewMonster()
         {
             _context ??= await MonsterDataContextFactory.CreateDbContextAsync();
@@ -34,6 +39,21 @@ namespace MonsterHunterDB.Pages
                 _context?.SaveChangesAsync();
             }
             ShowCreate = false;
+            await ShowMonsters();
+        }
+
+        //This Method Shows the List of our Monsters an example of Read//
+
+        public async Task ShowMonsters()
+        {
+            _context ??= await MonsterDataContextFactory.CreateDbContextAsync();
+
+            if (_context is not null)
+            {
+                OurMonsters = await _context.Monsters.ToListAsync();
+            }
+
+            if (_context is not null) await _context.DisposeAsync();
         }
     }
 
